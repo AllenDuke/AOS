b95f33c1f686c36a8ecc8130e017200d3b6de445
杜科 <1264310546@qq.com>
2021-01-13 13:52:13 +0800

    修改tty_test，准备origin

--- src/kernel/origin/origin.c
+++ src/kernel/origin/origin.c
@@ -5,13 +5,28 @@
 #include "core/kernel.h"
 #include "stdio.h"
 
+PRIVATE void exec_cmd(int cmdLen,char* cmdBuf);
+
 void origin() {
     int fd_stdin = open("/dev_tty0", O_RDWR);
-//    assert(fd_stdin == 0);
+    assert(fd_stdin == 0);
     int fd_stdout = open("/dev_tty0", O_RDWR);
-//    assert(fd_stdout == 1);
+    assert(fd_stdout == 1);
 
     printf("origin() is running ...\n");
+
+    char cmdBuf[128];
+    u32_t cmdLen = 0;
+
+    while (1) {
+        printf("$ ");
+        int r = read(fd_stdin, cmdBuf, 70);
+        cmdLen = r;
+        cmdBuf[r] = 0;
+
+        exec_cmd(cmdLen,cmdBuf);
+        cmdLen=0;
+    }
 
 
     int pid = fork();
@@ -29,4 +44,87 @@ void origin() {
         printf("child (%d) exited with status: %d.\n", child, s);
     }
 
+}
+
+PRIVATE void exec_cmd(int cmdLen,char* cmdBuf) {
+    if (cmdLen == 0) return;
+    int hash = 0;
+
+    /**
+     * 类似java String hash值计算，不直接采取31是因为cmd一般比较短，而31又比较小，
+     * 127这个质数比较大，可尽可能地使得hash的高位有值
+     * hash=hash*127+cmd[i]
+     */
+    for (int i = 0; i < cmdLen; i++) {
+        hash = ((hash << 8) - hash) + cmdBuf[i];
+    }
+
+    /**
+     * 扰动函数
+     * 同java HashMap hash()
+     * hash值的高16位为原hash的高16位，低16位为原hash高16位与原hash低16位异或后的结果。
+     * 原因：因为数组的长度为2的次方数，所以最终计算下标时，使用(NR_CMDS-1)&hash，
+     * 这样一来，因为(NR_CMDS-1)的高位总是0，hash的高位没有参与到运算，为了进一步降低冲突，应使得高位也参与运算
+     * 在权衡质量与速度后，选择了这种方式。成本不高，质量也不错。
+     */
+    hash = hash ^ (hash >> 16); /* int类型 >> 符号位保持不变 */
+    int index = hash & (NR_CMDS - 1);
+
+    char *cmd_map[NR_CMDS] = {
+            "",
+            "",
+            "pwd",
+            "date",
+            "",
+            "",
+            "",
+            "",
+    };
+
+    char *pre = cmd_map[index];
+    for (int i = 0; i < cmdLen; i++) {
+        if (cmdBuf[i] != *pre || *pre == '\0') {
+            kprintf("no such cmd:%s, hash:%d, index:%d\n", cmdBuf, hash, index);
+            return;
+        }
+        pre++;
+    }
+    //todo fork出一条进程去执行cmd
+    switch (index) {
+        case 0: {
+            printf("default cmd\n");
+            break;
+        }
+        case 1: {
+            printf("default cmd\n");
+            break;
+        }
+        case 2: {
+            printf("/\n");
+            break;
+        }
+        case 3: {
+            Message msg;
+            msg.source = TTY_TASK;
+            msg.type = GET_TIME;
+            send_rec(CLOCK_TASK, &msg);
+            printf("current date is: %d\n\r", msg.CLOCK_TIME);
+        }
+        case 4: {
+            printf("default cmd\n");
+            break;
+        }
+        case 5: {
+            printf("default cmd\n");
+            break;
+        }
+        case 6: {
+            printf("default cmd\n");
+            break;
+        }
+        case 7: {
+            printf("default cmd\n");
+            break;
+        }
+    }
 }
--- src/kernel/tty/tty.c
+++ src/kernel/tty/tty.c
@@ -136,21 +136,6 @@ PRIVATE void tty_dev_read(TTY *p_tty) {
     if (is_cur_console(p_tty->p_console)) keyboard_read(p_tty);
 }
 
-PRIVATE char cmd[TTY_IN_BYTES];
-PRIVATE u32_t cmdLen = 0;
-PRIVATE char *cmd_map[NR_CMDS] = {
-        "",
-        "",
-        "pwd",
-        "date",
-        "",
-        "",
-        "",
-        "",
-};
-
-PRIVATE void exec_cmd();
-
 PRIVATE void tty_dev_write(TTY *tty) {
     while (tty->inBufCount) {
         char ch = *(tty->p_inBufTail);
@@ -183,77 +168,6 @@ PRIVATE void tty_dev_write(TTY *tty) {
                 send(tty->tty_caller, &msg);
                 tty->tty_left_cnt = 0;
             }
-        }
-    }
-}
-
-PRIVATE void exec_cmd() {
-    if (cmdLen == 0) return;
-    int hash = 0;
-
-    /**
-     * 类似java String hash值计算，不直接采取31是因为cmd一般比较短，而31又比较小，
-     * 127这个质数比较大，可尽可能地使得hash的高位有值
-     * hash=hash*127+cmd[i]
-     */
-    for (int i = 0; i < cmdLen; i++) {
-        hash = ((hash << 8) - hash) + cmd[i];
-    }
-
-    /**
-     * 扰动函数
-     * 同java HashMap hash()
-     * hash值的高16位为原hash的高16位，低16位为原hash高16位与原hash低16位异或后的结果。
-     * 原因：因为数组的长度为2的次方数，所以最终计算下标时，使用(NR_CMDS-1)&hash，
-     * 这样一来，因为(NR_CMDS-1)的高位总是0，hash的高位没有参与到运算，为了进一步降低冲突，应使得高位也参与运算
-     * 在权衡质量与速度后，选择了这种方式。成本不高，质量也不错。
-     */
-    hash = hash ^ (hash >> 16); /* int类型 >> 符号位保持不变 */
-    int index = hash & (NR_CMDS - 1);
-
-    char *pre = cmd_map[index];
-    for (int i = 0; i < cmdLen; i++) {
-        if (cmd[i] != *pre || *pre == '\0') {
-            kprintf("no such cmd:%s, hash:%d, index:%d\n", cmd, hash, index);
-            return;
-        }
-        pre++;
-    }
-    //todo fork出一条进程去执行cmd
-    switch (index) {
-        case 0: {
-            kprintf("default cmd\n");
-            break;
-        }
-        case 1: {
-            kprintf("default cmd\n");
-            break;
-        }
-        case 2: {
-            kprintf("/\n");
-            break;
-        }
-        case 3: {
-            ttyMsg.source = TTY_TASK;
-            ttyMsg.type = GET_TIME;
-            send_rec(CLOCK_TASK, &ttyMsg);
-            kprintf("current date is: %d\n\r", ttyMsg.CLOCK_TIME);
-        }
-        case 4: {
-            kprintf("default cmd\n");
-            break;
-        }
-        case 5: {
-            kprintf("default cmd\n");
-            break;
-        }
-        case 6: {
-            kprintf("default cmd\n");
-            break;
-        }
-        case 7: {
-            kprintf("default cmd\n");
-            break;
         }
     }
 }
